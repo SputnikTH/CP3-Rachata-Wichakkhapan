cf6d8222a1a7d2b1065d27b9a09e45d7e3a1b584
SputnikTH <105562195+SputnikTH@users.noreply.github.com>
2022-05-17 23:20:16 +0700

    Create Lecture_50_Rachata_W.py

---
+++ Lecture_50_Rachata_W.py
@@ -0,0 +1,15 @@
+def addNumber(x,y):
+    print(x+y)
+addNumber(100,50)
+def addNumber(x,y):
+    print(x-y)
+addNumber(100,50)
+def addNumber(x,y):
+    print(x*y)
+addNumber(100,50)
+def addNumber(x,y):
+    print(int(x/y))
+addNumber(100,50)
+
+
+
